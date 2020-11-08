e792329e6992a3136ac3b38e0cf9365c32bd0b79
Siddharth Gelera <sidgroks@gmail.com>
2020-11-08 15:18:12 +0530

    test another option

--- knexfile.js
+++ knexfile.js
@@ -17,7 +17,7 @@ module.exports = {
 
   staging: {
     client: 'postgresql',
-    connection: process.env.DATABASE_URL + '?sslmode=require',
+    connection: process.env.DATABASE_URL + '?ssl=true',
     pool: {
       min: 2,
       max: 10
@@ -29,7 +29,7 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: process.env.DATABASE_URL + '?sslmode=require',
+    connection: process.env.DATABASE_URL + '?ssl=true',
     pool: {
       min: 2,
       max: 10
