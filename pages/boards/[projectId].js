53c3b6956f8482ddf7853cd5d3bbbfe6a356cd98
Siddharth Gelera <sidgroks@gmail.com>
2020-11-15 17:24:56 +0530

    boards base plate

--- pages/boards/[projectId].js
+++ pages/boards/[projectId].js
@@ -2,14 +2,20 @@ import Padding from 'components/padding'
 import AuthNav from 'components/auth-nav'
 import Spacer from 'components/spacer'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 export default function BoardByProjectId ({ ...props }) {
+  const router = useRouter()
+  const { projectId } = router.query
+
   return <>
     <Padding all={2}>
       <AuthNav />
       <h1>Boards</h1>
       <Spacer y={1} />
-      <section />
+      <section>
+        {projectId}
+      </section>
     </Padding>
   </>
 }
